21226196192508c3de577cc58bab0beb90ab4393
Nick Honings <i409398@fhict.nl>
2022-06-15 15:44:00 +0200

    changed response type to valid json response

--- packages/finder/finder/__main__.py
+++ packages/finder/finder/__main__.py
@@ -7,5 +7,5 @@ def main(args):
     translator = Translator()
     language = translator.detect(sentence, dest=destination)
     translated = translator.translate(sentence, dest=destination)
-
-    return {"body": {language.lang: translated.text}}
+    body = "Language detected: {0} | Translation: {1}".format(language.lang, translated.text)
+    return {"body":  body}
